f09e14e66970ac775bfaf1ec063af65184ef9e25
karalix <ducrosalix@hotmail.fr>
2014-10-25 20:18:36 +0200

    adding JSON reader class

---
+++ app/src/main/java/fr/coding_ops/t2cradar/modele/dataloader/JSONReader.java
@@ -0,0 +1,18 @@
+package fr.coding_ops.t2cradar.modele.dataloader;
+
+import java.io.InputStream;
+import java.util.List;
+
+import fr.coding_ops.t2cradar.R;
+import fr.coding_ops.t2cradar.modele.Arret;
+
+/**
+ * Created by Alix on 25/10/2014.
+ */
+public class JSONReader {
+    public List<Arret> readArrets()
+    {
+        //InputStream is = new InputStream(R.raw.arrets);
+        return null ;
+    }
+}
